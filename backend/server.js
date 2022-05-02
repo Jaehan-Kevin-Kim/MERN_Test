59f013c6b978daddd255e978a3f19f615a895121
Jaehan-Kevin-Kim <jhkevin21@gmail.com>
2022-05-01 18:56:59 -0600

    Use environment variable, and created first get route by using express.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,19 @@
-console.log('hello world');
+const express = require("express");
+const dotenv = require("dotenv").config();
+const port = process.env.PORT || 5001;
+
+const app = express();
+
+app.get("/api/goals", (req, res) => {
+  //   res.send("Get goals");
+
+  //   res.json({
+  //     message: "Get goals",
+  //   });
+
+  res.status(201).json({
+    message: "Get goals",
+  });
+});
+
+app.listen(port, () => console.log(`Server started on part ${port}`));
