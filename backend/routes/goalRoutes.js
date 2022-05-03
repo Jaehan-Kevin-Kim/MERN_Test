98a69b828f9e40e85eb352d2a35c63fc13702732
Jaehan-Kevin-Kim <jhkevin21@gmail.com>
2022-05-02 19:09:31 -0600

    Implemented controller function via separate controller module. And used connecting controller function to handle same route request with one line of code.

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,32 +1,48 @@
 const express = require("express");
 const router = express.Router();
+const {
+  getGoals,
+  postGoal,
+  updateGoal,
+  deleteGoal,
+} = require("../controllers/goalController");
+
+router.route("/").get(getGoals).post(postGoal);
+// router.route("/:id").put(updateGoal).delete(deleteGoal);
+
+router.route("/:id").put(updateGoal);
+router.route("/:name").delete(deleteGoal);
 
 //GET /api/goals
-router.get("/", (req, res) => {
-  res.status(200).json({
-    message: "Get goals in router module",
-  });
-});
+// router.get("/", getGoals);
+// router.get("/", (req, res) => {
+//   res.status(200).json({
+//     message: "Get goals in router module",
+//   });
+// });
 
 //POST /api/goals
-router.post("/", (req, res) => {
-  res.status(200).json({
-    message: "Create(Set) goal in router module",
-  });
-});
+// router.post("/", postGoal);
+// router.post("/", (req, res) => {
+//   res.status(200).json({
+//     message: "Create(Set) goal in router module",
+//   });
+// });
 
 //PUT /api/goals/:id
-router.put("/:id", (req, res) => {
-  res.status(200).json({
-    message: `Update goal ${req.params.id} in router module `,
-  });
-});
+// router.put(":/id", updateGoal);
+// router.put("/:id", (req, res) => {
+//   res.status(200).json({
+//     message: `Update goal ${req.params.id} in router module `,
+//   });
+// });
 
 //DELETE /api/goals/:name
-router.delete("/:name", (req, res) => {
-  res.status(200).json({
-    message: `Delete goal ${req.params.name} in router module`,
-  });
-});
+// router.delete(":/name", deleteGoal);
+// router.delete("/:name", (req, res) => {
+//   res.status(200).json({
+//     message: `Delete goal ${req.params.name} in router module`,
+//   });
+// });
 
 module.exports = router;
