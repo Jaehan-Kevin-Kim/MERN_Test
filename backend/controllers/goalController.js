04d3b9064a89a0d2bd6ff3e9c5839e29a4d10d0d
Jaehan-Kevin-Kim <jhkevin21@gmail.com>
2022-05-03 22:44:07 -0600

    Installed express-async-handler to handle async catch error handling. And applied this function to all requests from goalController

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,16 +1,18 @@
+const asyncHandler = require("express-async-handler");
+
 // @desc Get goals
 // @route GET /api/goals
 // @access Private
-const getGoals = (req, res) => {
+const getGoals = asyncHandler(async (req, res) => {
   res.status(200).json({
     message: "Get goals in controller",
   });
-};
+});
 
 // @desc Set goal
 // @route POST /api/goals
 // @access Private
-const setGoal = (req, res) => {
+const setGoal = asyncHandler(async (req, res) => {
   // console.log(req.body);
 
   if (!req.body.text) {
@@ -24,25 +26,26 @@ const setGoal = (req, res) => {
   res.status(200).json({
     message: `Create(Set) goal in contoller`,
   });
-};
+});
 
 // @desc Update goal
 // @route PUT /api/goals/:id
 // @access Private
-const updateGoal = (req, res) => {
+const updateGoal = asyncHandler(async (req, res) => {
   res.status(200).json({
     message: `Update goal ${req.params.id} in controller`,
   });
-};
+});
 
 // @desc Delete goal
 // @route DELETE /api/goals/:name
 // @access Private
-const deleteGoal = (req, res) => {
+const deleteGoal = asyncHandler(async (req, res) => {
   res.status(200).json({
     message: `Delete goal ${req.params.name} in controller`,
   });
-};
+});
+
 module.exports = {
   getGoals,
   setGoal,
