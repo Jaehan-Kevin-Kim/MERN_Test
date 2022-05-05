f5b2fa0a05152b1765e48c3c11c2b9aefad2bdd1
Jaehan-Kevin-Kim <jhkevin21@gmail.com>
2022-05-04 21:43:52 -0600

    Connected to MongoDB with Mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+const colors = require("colors");
 const { errorHandler } = require("./middleware/errorMiddleware");
+const connectDB = require("./config/db");
 const port = process.env.PORT || 5001;
+
 const app = express();
 const goalRouter = require("./routes/goalRoutes");
+
+connectDB();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
