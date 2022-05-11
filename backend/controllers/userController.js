207fd683a19812ad0ca0ca2f7910da0af611dfed
Jaehan-Kevin-Kim <jhkevin21@gmail.com>
2022-05-10 22:32:55 -0600

    Authenticate User, Generated JWT

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -6,7 +6,6 @@ const User = require("../models/userModel");
 // @desc Register new user
 // @route POST /api/users
 // @access Public
-
 const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body;
   console.log(name, email, password);
@@ -42,6 +41,7 @@ const registerUser = asyncHandler(async (req, res) => {
       _id: user.id,
       name: user.name,
       email: user.email,
+      token: generateToken(user._id),
     });
   } else {
     res.status(400);
@@ -57,9 +57,24 @@ const registerUser = asyncHandler(async (req, res) => {
 // @route POST /api/users/login
 // @access Public
 const loginUser = asyncHandler(async (req, res) => {
-  res.json({
-    message: "Login User",
-  });
+  const { email, password } = req.body;
+
+  // Check for user email (해당 email이 있는 user가 있는지 먼저 파악)
+  const user = await User.findOne({ email });
+
+  // User가 있는 경우 입력한 password가 맞는지 확인 하는 부분 (해당 password를 다시 salt, hash로 만들어 비교할 필요 없이 compare이라는 method를 사용 하면 됨)
+  if (user && (await bcrypt.compare(password, user.password))) {
+    // password는 지금 user가 입력한 password이고, user는 db에 hashed 형태로 저장된 해당 user의 password임.
+    res.json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid credentials");
+  }
 });
 
 // @desc Get user user
@@ -70,6 +85,13 @@ const getMe = asyncHandler(async (req, res) => {
     message: "User data display",
   });
 });
+
+// Generate JWT (JWT 생성하는 method)
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+};
 
 module.exports = {
   registerUser,
