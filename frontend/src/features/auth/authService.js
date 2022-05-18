3337d43f50408131eb6564a39ac50a87737242ad
Jaehan-Kevin-Kim <jhkevin21@gmail.com>
2022-05-17 23:22:50 -0600

    Hookup register form

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -1,16 +1,17 @@
 import axios from "axios";
 
-const API_URL = "/api/users/";
+const API_URL = "api/users/";
 
 // Register user
 const register = async (userData) => {
   const response = await axios.post(API_URL, userData);
 
   if (response.data) {
+    //axios를 사용하면 response값은 response.data에 저장 됨.
     localStorage.setItem("user", JSON.stringify(response.data));
   }
 
-  return response.userData;
+  return response.data;
 };
 
 const authService = {
--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -5,7 +5,8 @@ import authService from "./authService";
 const user = JSON.parse(localStorage.getItem("user"));
 
 const initialState = {
-  user: user ? user : null,
+  // user: user ? user : null,
+  user: user ?? null,
   isError: false,
   isSuccess: false,
   isLoading: false,
@@ -14,20 +15,20 @@ const initialState = {
 
 // Register user
 export const register = createAsyncThunk(
-  "auth/register",
+  "auth/register", // string with the action
   async (user, thunkAPI) => {
     try {
       return await authService.register(user);
     } catch (error) {
       const message =
         error.response?.data?.message || error.message || error.toString();
-      return thunkAPI.rejectWithValue(message);
+      return thunkAPI.rejectWithValue(message); //이렇게 해 주면, error 발생 시 해당 요청을 reject 해 주고, message를 payload에 넣어 줌.
     }
   },
 );
 
 export const authSlice = createSlice({
-  name: "auth",
+  name: "auth", // slice이름
   initialState,
   reducers: {
     // 여기 기재되는 값은 전부 synchronous 동작일 예정
@@ -40,6 +41,7 @@ export const authSlice = createSlice({
     },
   },
   extraReducers: (builder) => {
+    // 비동기 / thunk function들이 여기 기재 됨.
     builder
       .addCase(register.pending, (state) => {
         // 로딩 시
@@ -47,6 +49,7 @@ export const authSlice = createSlice({
       })
       .addCase(register.fulfilled, (state, action) => {
         // 성공 시
+        // 성공 시에는 돌려 받는 data가 있기 때문에 action을 추가 해 주기
         state.isLoading = false;
         state.isSuccess = true;
         state.user = action.payload; // 해당 action.payload는 위 register method에서 성공 시 return await authService.register(user) 을 통해 user를 return 해줬음. 따라서 action.payload의 값은 user임.
@@ -61,5 +64,8 @@ export const authSlice = createSlice({
   }, // 여기 기재되는 값은 전부 asynchronous 동작일 예정
 });
 
+// 위와 같이 authSlice의 reducers에 있는 값들을 아래와 같이 기재 해 줘야 함.
 export const { reset } = authSlice.actions;
+
+// 아래는 최종적으로 다른 곳에서 사용할 수 있도록 export 하는 방법
 export default authSlice.reducer;
