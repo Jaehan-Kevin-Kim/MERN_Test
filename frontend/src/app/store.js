43dd1a7a6b958edbcbd67a8b0ca97933598d638f
Jaehan-Kevin-Kim <jhkevin21@gmail.com>
2022-05-21 09:13:56 -0600

    Created GoalsSlice, GoalsService, and update store.js'

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/auth/authSlice";
+import goalReducer from "../features/goals/goalsSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    goals: goalReducer, // 이렇게 goals라고 쓰든, goal이라고 쓰든 상관없음. 왜냐하면 goalReducer라는 이름으로 goalsSlice전체를 import했고, 그 값에대한 변수지명을 해준것과 마찬가지기 때문에, 해당 이름이 어떤것이던 간에 크게 상관없음.
   },
 });
---
+++ frontend/src/features/goals/goalsService.js
@@ -0,0 +1,7 @@
+import axios from "axios";
+
+const API_URL = "api/goals/";
+
+const goalsService = {};
+
+export default goalsService;
