6f8e0555003d25927fa1095d6f0db819ea5d3755
Jaehan-Kevin-Kim <jhkevin21@gmail.com>
2022-05-23 10:00:43 -0600

    Added production mode configuration in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const dotenv = require("dotenv").config();
 const colors = require("colors");
@@ -16,6 +17,25 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/users", userRouter);
 app.use("/api/goals", goalRouter);
+
+// Serve frontend (Frontend 연결을 위해 build가 된 frontend에 (build > index.html) 접근 할 수 있도록 설정 해 줌.)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build"))); // frontend > build 폴더에 접근 하게 해 줌
+  app.get(
+    "*",
+    (
+      req,
+      res, // frontend의 index.html 에 접근하게 해주는 코드
+    ) =>
+      res.sendFile(
+        path.resolve(__dirname, "../", "frontend", "build", "index.html"),
+      ),
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("Please set to production");
+  });
+}
 
 app.use(errorHandler);
 
